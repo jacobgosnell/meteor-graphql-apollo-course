426ac9bb90d8efe4abcad3f79c42e08c26c0f8e2
Jake Gosnell <jake@hangar31development.com>
2019-12-29 10:46:45 -0800

    graphql queries in react turn query responses into react properties

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,5 +1,16 @@
 import React from "react";
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
-const App = () => <h1>Hello</h1>;
 
-export default App;
+const App = ({data}) => <h1>{data.hi}</h1>;
+
+const hiQuery = gql` 
+{
+  hi
+}
+`;
+
+export default graphql(
+  hiQuery
+)(App);
