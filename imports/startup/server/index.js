89b24ba728a6febaa5f46d5cbfa3c4aeca0e324d
Jake Gosnell <jake@hangar31development.com>
2019-12-28 22:51:42 -0800

    built first schema & query

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,4 +1,25 @@
 import { createApolloServer } from 'meteor/apollo';
 import { makeExecutableSchema } from 'graphql-tools';
 
-createApolloServer({});
+//schema that defines data in database
+const typeDefs = `
+  type Query {
+    hi: String
+  }
+`;
+
+// hits database like meteor method
+const resolvers = {
+  Query: {
+    hi() {
+      return "Hello Level Up";
+    }
+  }
+};
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+
+createApolloServer({ schema });
